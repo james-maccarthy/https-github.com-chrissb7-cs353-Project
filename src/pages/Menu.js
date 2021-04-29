e2d236f7c734ffb27ce0840971d349bf72512f84
Gang <15666671996@163.com>
2021-04-29 16:43:21 +0100

    implemented render meals in JSON to Menu Page and related UI

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,6 +1,8 @@
 import React, {Component} from "react";
 import "./Menu.css";
 import arrow from "../img/arrow.png";
+import meals from "../jsonData/FoodMenu.json";
+import $ from "jquery";
 
 class Menu extends Component {
     constructor(props) {
@@ -8,47 +10,101 @@ class Menu extends Component {
         // set two state variable to handle user current page and sidebar toggle
         this.state = {
             menuStatus: [false, false, false, false, false, false, false],
+            mealSelected: null
         };
+        $("body").click(function () {
+            $("#daySelector").hide();
+        });
     }
 
     menuClick(index) {
         let newMenuStatus = [false, false, false, false, false, false, false];
         newMenuStatus[index] = !this.state.menuStatus[index];
         this.setState({menuStatus: newMenuStatus});
+    }
 
+    renderMeals(cuisineName) {
+        var cuisine = meals[cuisineName];
+        var h = (cuisine.Breakfast.length + cuisine.Lunch.length + cuisine.Dinner.length + 3) * 4.3;
+        $(":root").css("--box-height", h + "vh");
+        return (<div>
+            <div className="mealTime"><strong>Breakfast</strong></div>
+            {cuisine.Breakfast.map((meal, index) => {
+                return <div key={index} className="meal">
+                    <p>{meal.name}</p>
+                    <button type="button" onClick={this.addButtonClick.bind(this)}>+</button>
+                </div>
+            })}
+            <div className="mealTime"><strong>Lunch</strong></div>
+            {cuisine.Lunch.map((meal, index) => {
+                return <div key={index} className="meal">
+                    <p>{meal.name}</p>
+                    <button type="button" onClick={this.addButtonClick.bind(this)}>+</button>
+                </div>
+            })}
+            <div className="mealTime"><strong>Dinner</strong></div>
+            {cuisine.Dinner.map((meal, index) => {
+                return <div key={index} className="meal">
+                    <p>{meal.name}</p>
+                    <button type="button" onClick={this.addButtonClick.bind(this)}>+</button>
+                </div>
+            })}
+        </div>);
+    }
+
+    addButtonClick(event) {
+        event.stopPropagation();
+        this.setState({mealSelected: $(event.target).parent().find("p").text()});
+        $("#daySelector").css({
+            "top": event.target.offsetTop - 1 - event.target.offsetHeight / 2 + "px",
+            "left": event.target.offsetLeft + event.target.offsetWidth + "px"
+        }).show();
+    }
+
+    mealSelector(event) {
+        var day = $(event.target).text();
+        var meal = this.state.mealSelected;
+        console.log(meal + " add into " + day);
     }
 
     render() {
         return (
             <div>
                 <div className="menus" onClick={this.menuClick.bind(this, 0)}>
-                    <p className="cuisine">Chinese</p>
+                    <p className="cuisine">Europe</p>
                     <img src={arrow} className={this.state.menuStatus[0] ? "arrow-active" : "arrow"} alt=""/>
                 </div>
                 <div className={this.state.menuStatus[0] ? "boxes-active" : "boxes"}>
+                    {this.renderMeals("Europe")}
                 </div>
                 <div className="menus" onClick={this.menuClick.bind(this, 1)}>
-                    <p className="cuisine">British</p>
+                    <p className="cuisine">North America</p>
                     <img src={arrow} className={this.state.menuStatus[1] ? "arrow-active" : "arrow"} alt=""/>
                 </div>
                 <div className={this.state.menuStatus[1] ? "boxes-active" : "boxes"}>
+                    {this.renderMeals("North America")}
                 </div>
                 <div className="menus" onClick={this.menuClick.bind(this, 2)}>
-                    <p className="cuisine">American</p>
+                    <p className="cuisine">Asia</p>
                     <img src={arrow} className={this.state.menuStatus[2] ? "arrow-active" : "arrow"} alt=""/>
                 </div>
                 <div className={this.state.menuStatus[2] ? "boxes-active" : "boxes"}>
+                    {this.renderMeals("Asia")}
                 </div>
-                <div className="menus" onClick={this.menuClick.bind(this, 3)}>
-                    <p className="cuisine">Japanese</p>
-                    <img src={arrow} className={this.state.menuStatus[3] ? "arrow-active" : "arrow"} alt=""/>
-                </div>
-                <div className={this.state.menuStatus[3] ? "boxes-active" : "boxes"}>
+                <div id="daySelector">
+                    <div onClick={this.mealSelector.bind(this)}>Monday</div>
+                    <div onClick={this.mealSelector.bind(this)}>Tuesday</div>
+                    <div onClick={this.mealSelector.bind(this)}>Wednesday</div>
+                    <div onClick={this.mealSelector.bind(this)}>Thursday</div>
+                    <div onClick={this.mealSelector.bind(this)}>Friday</div>
+                    <div onClick={this.mealSelector.bind(this)}>Saturday</div>
+                    <div onClick={this.mealSelector.bind(this)}>Sunday</div>
                 </div>
 
                 <div id="counter">
                     <p>Open Calories Counter</p>
                 </div>
+
             </div>
         );
     }
