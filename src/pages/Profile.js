22ddcd4451908945f932c2ef81851d82fe24e58e
Gang <15666671996@163.com>
2021-05-06 18:29:39 +0100

    local data problem fix

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,22 +2,25 @@ import React, { Component } from "react";
 import "./Profile.css";
 import userSelected from "../jsonData/localData.json";
 
-// const user = require("../jsonData/localData.json");
 
-const newdata = 
-userSelected.map((user, index)=>{return(parseInt(user.meal.calorie))})
-
-const totalCal= 
-newdata.reduce((acc, cals)=>{return acc+cals}, 0);
-
+// let newdata = userSelected.map((user, index)=>{return(parseInt(user.meal.calorie))});
+// let totalCal= newdata.reduce((acc, cals)=>{return acc+cals}, 0);
 
 class Profile extends Component {
-  
+
+
+    test123() {
+        console.log(userSelected);
+        let newdata = userSelected.map((user, index)=>{return(parseInt(user.meal.calorie))});
+        let totalCal= newdata.reduce((acc, cals)=>{return acc+cals}, 0);
+        return totalCal;
+    }
     render() {
         return (
             <div>
-            <div>Goals</div>
-            <div>{totalCal}</div>
+                <div>Goals</div>
+                {/*<div>{totalCal}</div>*/}
+                <div>{this.test123()}</div>
             </div>
         );
     }
