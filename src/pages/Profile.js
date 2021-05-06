1d05a13eeb35f0a16c124e6913247cc297433240
Christopher Boland <chrissbb90@gmail.com>
2021-05-06 18:14:11 +0100

    Local data issue 2

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,24 @@
 import React, { Component } from "react";
 import "./Profile.css";
+import userSelected from "../jsonData/localData.json";
+
+// const user = require("../jsonData/localData.json");
+
+const newdata = 
+userSelected.map((user, index)=>{return(parseInt(user.meal.calorie))})
+
+const totalCal= 
+newdata.reduce((acc, cals)=>{return acc+cals}, 0);
+
 
 class Profile extends Component {
+  
     render() {
         return (
-            <div>user profile</div>
+            <div>
+            <div>Goals</div>
+            <div>{totalCal}</div>
+            </div>
         );
     }
 }
